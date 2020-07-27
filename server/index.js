8293f9f3c9e22b3148336b8eb14fbdef983e6995
Mrrob222 <misha1010203@gmail.com>
2020-07-27 16:24:09 +0300

    added mongoose & nodemon

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const PORT = 3000;
+const mongoose = require('mongoose');
+
 const app = express();
+const PORT = 3000;
+
+mongoose.connect('mongodb+srv://Mrrob:Misha1234321@cluster0.muqng.mongodb.net/server_starterPack?retryWrites=true&w=majority',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+});
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
 
 app.listen(PORT, error => {
     error ? console.log(error): console.log(`server started on port ${PORT}`)
-})
+});
